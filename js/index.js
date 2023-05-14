89b59b4443cecde889b3d82844cf7cb2ba58e7db
Salvador Rodriguez <ale2ph@hotmail.com>
2023-05-14 11:05:44 -0600

    agregado de ejemplo de simple radio get value y del acceso a los forms objects en todo el document

--- js/index.js
+++ js/index.js
@@ -2,7 +2,14 @@ const elementForm = document.querySelector("#formBox");
 
 elementForm.addEventListener("submit",(event)=>{
     event.preventDefault();
-    const hiTextInput = event.target.hiText
+    const hiTextInput = event.target.hiText.value
     console.log(hiTextInput);
     console.log(event)
+    const niuShit = document.querySelector("input[type='radio'][name=beltRadio]:checked").value;
+    console.log(niuShit);
 })
+
+const loginForm = document.forms.login; // Or document.forms['login']
+    loginForm.elements.  
+    loginForm.elements.email.placeholder = "teasasst@example.com";
+    loginForm.elements.password.placeholder = "password";

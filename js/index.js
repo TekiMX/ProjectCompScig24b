0f697e8ebedbcc6f5a3a688dbe4117fe4a051ae8
Salvador Rodriguez <ale2ph@hotmail.com>
2023-05-16 21:24:20 -0600

    fix:modificado del json

--- js/index.js
+++ js/index.js
@@ -1,31 +1,17 @@
-//Variables y demas estof ai nid
+/**************************** Variables y demas estof ai nid ********************************/
 const elementFormRadios = document.querySelector("#formBoxRadios");
 const elementFormData = document.querySelector("#formBoxData");
+const DivCentral = document.querySelector("#divCenter")
 let techniques = [];
 let contactData =[];
 
-/************************** acceso a viernes13 ********************************/
-const main = () => {
-    fetch("./api/techniques.json")
+/************************** acceso a JSON ********************************/
+const main = () => {    
+        fetch('/api/techniques.json')
         .then(response => response.json())
-        .then(data => normalizeData(data))
+        .then(data => organizedData (data))
         .then(techniques => techniques.forEach(renderDivCentral))
-    };
-    
-    const organizedData = (data) => {
-        data.forEach(element => {
-            const {belt, type, nameK, nameMX, image} = element;
-            const techniquesSelected ={
-                belt: belt,
-                type: type, 
-                nameK: nameK,
-                nameMX: nameMX,
-                image: image
-            };
-            tecniques.push(techniquesSelected);
-        });
-    return techniques;
-    };
+};
 
 /******************************** forms ***************************************/
 /* Acceso a los datos del form de radios*/
@@ -35,7 +21,10 @@ elementFormRadios.addEventListener("submit",(event)=>{
     console.log(hiTextInput);*/
     console.log(event) 
     const niuShit = document.querySelector("input[type='radio'][name=beltRadio]:checked").value;
-    console.log(niuShit);
+    console.log("este es el querySelector del radio",niuShit);
+    console.log("este es mi techniques", techniques)
+    renderDivCentral(techniques);
+
 })
 //acceso a los datos del form de contacto
 elementFormData.addEventListener("submit",(event)=>{
@@ -48,3 +37,30 @@ elementFormData.addEventListener("submit",(event)=>{
     console.log(yourInterest);
 })
 
+/*********************** functions ***********************/
+//function renderDivCentral para insertar en el HMTL los divs con las propiedades del array
+const renderDivCentral = (element) => {
+    /* const {belt} = techniques;
+    belt.forEach(belt, index) => { */
+    const techDiv = document.createElement('div');
+    const imgkDiv= document.createElement('img');
+    console.log("este es mi element",element);
+    /* img.setAttribute("src", image); */                        //DESCOMENTAR
+
+    DivCentral.appendChild(techDiv);
+    techDiv.appendChild(imgkDiv);
+};
+//function organizedData para seleccionar los datos del json
+const organizedData = (data) => {
+    data.forEach(element => {
+        const {belt, image} = element;
+        console.log(belt);
+        const techniquesSelected ={
+            belt: belt,
+            image: image
+        };
+        console.log("este es mi techniquesSelected", techniquesSelected);
+        techniques.push(techniquesSelected);
+    });
+return techniques;
+};

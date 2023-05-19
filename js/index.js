f93f795e47557afa3b0b671f9310ce7085ae818a
Salvador Rodriguez <ale2ph@hotmail.com>
2023-05-18 22:19:57 -0600

    DOC:ai faund te lirel bastaaaarrrdddd

--- js/index.js
+++ js/index.js
@@ -6,12 +6,13 @@ let techniques = [];
 let contactData =[];
 
 /************************** acceso a JSON ********************************/
-const main = () => {    
-        fetch('/api/techniques.json')
+
+        fetch("./api/techniques.json")
         .then(response => response.json())
         .then(data => organizedData (data))
+        /* .then(data => console.log(data)) */
         .then(techniques => techniques.forEach(renderDivCentral))
-};
+        
 
 /******************************** forms ***************************************/
 /* Acceso a los datos del form de radios*/
@@ -19,10 +20,10 @@ elementFormRadios.addEventListener("submit",(event)=>{
     event.preventDefault();
     /* const hiTextInput = event.target.hiText.value
     console.log(hiTextInput);*/
-    console.log(event) 
+    /* console.log(event) */ 
     const niuShit = document.querySelector("input[type='radio'][name=beltRadio]:checked").value;
     console.log("este es el querySelector del radio",niuShit);
-    console.log("este es mi techniques", techniques)
+    /* console.log("este es mi techniques", techniques) */
     renderDivCentral(techniques);
 
 })
@@ -39,28 +40,42 @@ elementFormData.addEventListener("submit",(event)=>{
 
 /*********************** functions ***********************/
 //function renderDivCentral para insertar en el HMTL los divs con las propiedades del array
+
 const renderDivCentral = (element) => {
-    /* const {belt} = techniques;
-    belt.forEach(belt, index) => { */
     const techDiv = document.createElement('div');
-    const imgkDiv= document.createElement('img');
-    console.log("este es mi element",element);
-    /* img.setAttribute("src", image); */                        //DESCOMENTAR
+    /* const imgDiv = document.createElement('img'); */
+    /* console.log("este es el array del renderDivCentral",element); */
+    /* imgDiv.setAttribute("src", image);     */                    //DESCOMENTAR
 
     DivCentral.appendChild(techDiv);
-    techDiv.appendChild(imgkDiv);
+    /* techDiv.appendChild(imgkDiv); */
 };
 //function organizedData para seleccionar los datos del json
 const organizedData = (data) => {
     data.forEach(element => {
         const {belt, image} = element;
-        console.log(belt);
+        console.log(`here is belt object ${belt}`);
         const techniquesSelected ={
             belt: belt,
             image: image
         };
-        console.log("este es mi techniquesSelected", techniquesSelected);
+        /* console.log("este es mi techniquesSelected", techniquesSelected); */
         techniques.push(techniquesSelected);
     });
 return techniques;
 };
+
+
+/******************************* I N C O R P O R A R ********************************/
+// armar una estructura de datos, segun lo que necesitamos
+const language = {
+    description: inputLanguageElement.value,
+    status: statusElement.value
+}
+languages.push(language);
+// limpiar la vista anterior
+cleanView();
+// call funcion que crea y agrega los elementos nuevos.
+renderViewlanguages(languages);
+// actualizar total de elementos
+renderTotal();

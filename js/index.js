21857faf7063d5efc6df5819a9e2afda4bf6a94c
Salvador Rodriguez <ale2ph@hotmail.com>
2023-05-29 23:34:49 -0600

    feat:final website 1.0

--- js/index.js
+++ js/index.js
@@ -1,52 +1,84 @@
-//Variables y demas estof ai nid
-const elementFormRadios = document.querySelector("#formBoxRadios");
-const elementFormData = document.querySelector("#formBoxData");
-const DivCentral = document.querySelector("#divCenter");
-let techniques = []
-let contactData = []
-let objectsWithColor = []
-
 //main , acceso al json y presentado de datos
 const main = () => {
-    fetch("./api/techniques.json")
-    .then(response => response.json())
-    .then(data => organizedData(data))
-    .then(techniques => findObjectsByColor(techniques))
-    .then(objectsWithColor => objectsWithColor.forEach(renderTechniques))
-    .then(console.log("aqui esta el objectsWithColor", objectsWithColor))
+    //Variables y demas estof ai nid
+    const elementFormRadios = document.querySelector("#formBoxRadios");
+    const elementFormData = document.querySelector("#formBoxData");
+    const DivCentral = document.querySelector("#divCenter");
+    
+    // functions
+    const selectTechniques = async event => {
+        event.preventDefault();
+        const selectedColor = document.querySelector("input[type='radio'][name=beltRadio]:checked").value;
+        try {
+            // consulta api - json
+            const techniques = await getDataFromApiByColor(selectedColor);
+            console.log('elementos de color tal', techniques);
+            // render
+            clearView();
+            techniques.forEach(renderTechniques);
+            // comentar
+            // getDataFromApiByColor(selectedColor)
+            //     .then(techniques => {
+            //         clearView();
+            //         techniques.forEach(renderTechniques);
+            //         console.log(techniques)
+            //     })
+            console.log('hola estoy despues de todo')
+        } catch (error) {
+            throw new Error(error.stack)
+        }
+    };
+
+    const getDataFromApiByColor = async (color) => {
+
+        return new Promise((resolve, reject) => {
+            // piensao en utilidad y no en concepto
+            // que tengo que haces para hacer una consulta a una api?
+            fetch("./api/techniques.json")
+                .then(response => response.json())
+                .then(techniques => {
+                    const arr = findObjectsByColor(color, techniques)
+                    resolve(arr);
+                })
+                .catch(err => {
+                    reject(err);
+                });
+        })
+        /* async await */
+        const response = await fetch("./api/techniques.json");
+        const techniques = await response.json();
+        return findObjectsByColor(color, techniques);
+    };
+
+    const findObjectsByColor = (color, techniquesArr) => {
+        const objectsWithColor = techniquesArr.filter(obj => obj.belt === color);
+        return objectsWithColor;
+    };
+
+    const clearView = () => {
+        DivCentral.innerHTML = '';
+    };
+
+    const renderTechniques = (element) => {
+        // add elements
+        const divEl = document.createElement("div");
+        const imgEl = document.createElement("img");
+        imgEl.classList.add("imageClass");
+
+        imageURL = element.image
+        imgEl.setAttribute("src", imageURL);
+
+        DivCentral.appendChild(divEl);
+        DivCentral.appendChild(imgEl);
+        /* divEl.innerHTML += "div text"; */
+    }
+    
+    
+    // listeners
+    elementFormRadios.addEventListener("submit", selectTechniques);
+    // aqui van todos los listeners
+    // aca no pongas mas funciones
 };
-
-const organizedData = (data) => {
-    data.forEach(element => {
-        const {belt, image} = element;
-        const technique ={
-            belt: belt,
-            image: image
-        };
-        /* console.log("estos son los datos contenidos en technique", technique); */
-        techniques.push(technique);
-    });
-return techniques;
-console.log(`y aca estan esta el nuevo array con techniques ${techniques}`);
-};
-
-const findObjectsByColor = (color) => {
-    const objectsWithColor = techniques.filter(obj => obj.belt === color);
-    return objectsWithColor;
-  };
-  
-const renderTechniques = (element) => {
-    const divEl = document.createElement("div");
-    const imgEl = document.createElement("img");
-    img.classList.add("imageClass");
-
-    imageURL = element.image
-    img.setAttribute("src", imageURL);
-
-    divEl.appendChild(DivCentral);
-    imgEl.appendChild(divEl);
-
-}
 
 //lanza la aplicacion
 main();
